d7415d50752f63350698738eb7832a54d026097a
yogesh-ryogi <yogeshryogi2616@gmail.com>
2026-06-18 13:08:14 +0000

    dtsummary

---
+++ .devcontainer/datatypes-summary.js
@@ -0,0 +1,33 @@
+// for interview pov
+//primitive data types
+//7
+// String, Number, Boolean, Undefined, Null, Symbol, BigInt
+
+const score=100
+const scoreValue=100.89
+
+const isLoggedIn=true
+
+const outsideTemp=null
+
+let userEmail=undefined
+const id = symbol('123')
+const anotherId = symbol('123')
+console.log(id===anotherId);
+
+const bigNumber= 1234567890123456789012345678901234567890n
+
+
+//Refrence data types
+//Object, Array, Function, Date, RegExp, Map, Set, WeakMap, WeakSet
+
+const heroes=["spiderman","ironman"];
+
+let myObj={
+    name:"raj",
+    age:30,
+}
+
+function sayHello(){
+    console.log("Hello");
+}
