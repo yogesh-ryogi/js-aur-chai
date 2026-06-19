1d6e021781ad8b54b69aebd3be04a348c53b19c3
yogesh-ryogi <yogeshryogi2616@gmail.com>
2026-06-19 17:36:06 +0000

    zt

---
+++ .devcontainer/01_basics/05_strings.js
@@ -0,0 +1,9 @@
+const name="gowda"
+const repoCount=50
+
+console.log(name + repoCount+"value"); // not to use it as it is outdated and not readable
+
+//better to use template literals
+
+console.log('hello my name is ${name} and my repo count is ${repocount}');
+
