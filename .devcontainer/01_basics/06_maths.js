e4b2f4d5b5ce0ae5509d0414739bdb58440436d0
yogesh-ryogi <yogeshryogi2616@gmail.com>
2026-06-21 15:05:59 +0000

    maths

---
+++ .devcontainer/01_basics/06_maths.js
@@ -0,0 +1,17 @@
+console.log(Math)
+console.log(Math.abs(-5))
+console.log(Math.round(5.7))
+console.log(Math.ceil(5.1))
+console.log(Math.floor(5.9))
+console.log(Math.max(1, 5, 3))
+console.log(Math.min(1, 5, 3))
+console.log(Math.random())
+console.log(Math.sqrt(16))
+console.log(Math.pow(2, 3))
+console.log(Math.random() * 10) // Random number between 0 and 10
+console.log(Math.floor(Math.random() * 10)) // Random integer between 0 and 9
+
+const min=10
+const max=20
+const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum) // Random integer between 10 and 20  
