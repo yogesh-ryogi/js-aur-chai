4e92dd255f3956e54984a679a34175ff752973dd
yogesh-ryogi <yogeshryogi2616@gmail.com>
2026-06-23 06:13:20 +0000

    123

--- .devcontainer/01_basics/07_dates.js
+++ .devcontainer/01_basics/07_dates.js
@@ -16,3 +16,18 @@ let mycreateddate2 = new Date(2006,0,26,16,55,0);
 console.log(mycreateddate2);
 console.log(mycreateddate2.toDateString());
 console.log(mycreateddate2.toTimeString());
+
+let mytimestamp= Date.now();
+console.log(mytimestamp);
+console.log(mycreateddate.getTime());
+
+//in seconds
+console.log(Math.floor(mytimestamp/1000));
+console.log(Math.floor(mycreateddate.getTime()/1000));    
+
+
+let newdate=new Date();
+console.log(newdate);
+console.log(newdate.getMonth());
+console.log(newdate.getDate());
+console.log(newdate.getDay());
